9aae8f741d418fdc18b2442dfa942683a9836baf
timslg <mail@timschlegel.com>
2023-11-22 13:12:31 +0100

    add reverse proxy

--- qfin-backend/src/index.ts
+++ qfin-backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const port = 4000;
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
